c4535a74c3942c4090f55662ff858c5c62212e36
Mostakimw <scientist.expert@gmail.com>
2023-12-01 19:59:08 +0600

    team model and interface created

---
+++ src/app/modules/teams/team.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose'
+import { TMember, TTeam } from './team.interface'
+
+const teamMemberSchema = new Schema<TMember>(
+  {
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'User', // Reference to the User model
+    },
+  },
+  { _id: false },
+)
+
+const teamSchema = new Schema<TTeam>({
+  team_name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  members: {
+    type: [teamMemberSchema],
+    default: [],
+  },
+})
+
+export const Team = model<TTeam>('Team', teamSchema)
